2d477ca2c98eb5a729b31fda82f3ae6a6a25f54d
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-09-21 19:44:12 -0600

    adds game images to games display

--- src/components/GameDisplay.jsx
+++ src/components/GameDisplay.jsx
@@ -13,15 +13,22 @@ const GamesDisplay = ({ authToken }) => {
 
   return (
     <div className="add-timer">
-      <button onClick={handleDisplayGames}>Display Games</button>
+      <div>
+        <button onClick={handleDisplayGames}>Display Games</button>
+      </div>
       <div>
         <div className="timers-list-wrapper">
           {gamesList &&
             gamesList.map((gameData, gameId) => {
               return (
-                <div>
+                <div key={gameId}>
                   <h2>{gameData.name}</h2>
                   <p>{gameData.description}</p>
+                  <img
+                    src={gameData.image_url}
+                    alt={gameData.name + " image"}
+                    style={{ width: "200px" }}
+                  />
                 </div>
               );
             })}
