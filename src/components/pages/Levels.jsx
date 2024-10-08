05e416f14fc1148ac5a2f319117000078fb0e83f
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-10-07 18:56:41 -0600

    sets up Levels page

--- src/components/pages/Levels.jsx
+++ src/components/pages/Levels.jsx
@@ -1,10 +1,46 @@
-// import GamesDisplay from "../GameDisplay";
+import { useState } from "react";
+import fetchWrapper from "../../lib/apiCall";
 
-export default function Levels() {
+const Levels = () => {
+  const [levelsList, setLevelsList] = useState(null);
+
+  const handleDisplayLevels = () => {
+    fetchWrapper
+      .apiCall(`/elements`, "GET")
+      .then((response) =>
+        setLevelsList(
+          response.results.filter((element) => element.type.name == "Level")
+        )
+      )
+      .catch((error) => console.error("couldn't display levels", error));
+  };
+
   return (
-    <div className="login-container">
+    <div className="levels">
       <h1>Levels</h1>
-      {/* <GamesDisplay /> */}
+      <div>
+        <button onClick={handleDisplayLevels}>Display Levels</button>
+      </div>
+      <div>
+        <div className="timers-list-wrapper">
+          {levelsList &&
+            levelsList.map((levelData, levelId) => {
+              return (
+                <div key={levelId}>
+                  <h2>{levelData.name}</h2>
+                  <p>{levelData.description}</p>
+                  <img
+                    src={levelData.image_url}
+                    alt={levelData.name + " image"}
+                    style={{ width: "200px" }}
+                  />
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Levels;
