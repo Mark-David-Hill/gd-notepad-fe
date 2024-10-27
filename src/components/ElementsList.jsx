8dfd508cb0a93cc54d191c377dff003fa3eca2a8
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-10-27 16:19:02 -0600

    adjusts relationships display for game elements and adds images

--- src/components/ElementsList.jsx
+++ src/components/ElementsList.jsx
@@ -10,8 +10,7 @@ const GameElementsList = ({ elementsList, relationshipsList }) => {
           return (
             <div key={elementId}>
               <div>
-                <h2>Main Element Info</h2>
-                <h3>{elementData.name}</h3>
+                <h2>{elementData.name}</h2>
                 <p>{elementData.description}</p>
                 <img
                   src={elementData.image_url}
@@ -24,7 +23,7 @@ const GameElementsList = ({ elementsList, relationshipsList }) => {
               </div>
               {elementData.notes.length > 0 && (
                 <div>
-                  <h2>Notes</h2>
+                  <h3>Notes</h3>
                   {elementData.notes.map((note) => (
                     <p key={note.note_id}>{note.content}</p>
                   ))}
@@ -32,20 +31,39 @@ const GameElementsList = ({ elementsList, relationshipsList }) => {
               )}
 
               <div>
-                <h2>Relationships</h2>
+                {relationshipsList.reduce((count, relationship) => {
+                  return (
+                    count +
+                    (relationship.element_1.element_id ===
+                      elementData.element_id ||
+                    relationship.element_2.element_id === elementData.element_id
+                      ? 1
+                      : 0)
+                  );
+                }, 0) > 0 && <h3>Relationships</h3>}
                 {relationshipsList.map((relationship, relationshipId) =>
                   relationship.element_1.element_id ===
                   elementData.element_id ? (
                     <div key={relationshipId}>
-                      <p>{relationship.element_2.name}</p>
+                      <h4>{relationship.element_2.name}</h4>
                       <p>{relationship.description}</p>
+                      <img
+                        src={relationship.element_2.image_url}
+                        alt={relationship.element_2.name}
+                        style={{ width: "100px" }}
+                      />
                     </div>
                   ) : (
                     relationship.element_2.element_id ===
                       elementData.element_id && (
                       <div key={relationshipId}>
-                        <p>{relationship.element_1.name}</p>
+                        <h4>{relationship.element_1.name}</h4>
                         <p>{relationship.description}</p>
+                        <img
+                          src={relationship.element_1.image_url}
+                          alt={relationship.element_1.name}
+                          style={{ width: "100px" }}
+                        />
                       </div>
                     )
                   )
