2688a84066b82f6b904c2bf19d565a26ddd3ab5c
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-11-03 19:51:52 -0700

    sets up icons for view type select buttons

--- src/components/ElementsDisplay.jsx
+++ src/components/ElementsDisplay.jsx
@@ -25,9 +25,24 @@ const ElementsDisplay = ({ elementType }) => {
   return (
     <div className={"game-elements-wrapper"}>
       <div className="view-select-wrapper">
-        <button onClick={() => setViewType("square")}>Square</button>
-        <button onClick={() => setViewType("card")}>Card</button>
-        <button onClick={() => setViewType("row")}>Row</button>
+        <button
+          className={viewType === "square" ? "selected" : ""}
+          onClick={() => setViewType("square")}
+        >
+          <img src="https://cdn4.iconfinder.com/data/icons/176-material-design-outline-core/24/apps-512.png" />
+        </button>
+        <button
+          className={viewType === "card" ? "selected" : ""}
+          onClick={() => setViewType("card")}
+        >
+          <img src="https://cdn.iconscout.com/icon/free/png-256/free-storyboard-icon-download-in-svg-png-gif-file-formats--list-card-detail-vlogger-and-video-platform-pack-entertainment-icons-1222283.png" />
+        </button>
+        <button
+          className={viewType === "row" ? "selected" : ""}
+          onClick={() => setViewType("row")}
+        >
+          <img src="https://static.thenounproject.com/png/2250454-200.png" />
+        </button>
       </div>
 
       <h1>{elementType}s</h1>
