420f5c0e4b05858d7d2058ccb96d6fcaec69fb99
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-11-17 19:12:41 -0700

    adds styling to the add game element form

--- src/components/ElementsDisplay.jsx
+++ src/components/ElementsDisplay.jsx
@@ -114,52 +114,61 @@ const ElementsDisplay = ({ elementType }) => {
       </div>
 
       <h1>{elementType}s</h1>
-      <div className="add-element-form">
-        <select name="" id="" onChange={handleSetFormGameId} value={formGameId}>
-          <option value="">Select Game</option>
-          {gamesList &&
-            gamesList.map((game) => (
-              <option key={game.game_id} value={game.game_id}>
-                {game.name}
-              </option>
-            ))}
-        </select>
-        <input
-          type="text"
-          placeholder="name"
-          value={formName}
-          onChange={handleSetFormName}
-        />
-        <input
-          type="text"
-          placeholder="image-url"
-          value={formImgUrl}
-          onChange={handleSetFormImgUrl}
-        />
-        <textarea
-          name="form-description"
-          id="form-description"
-          placeholder="description"
-          value={formDescription}
-          onChange={handleSetFormDescription}
-        ></textarea>
-        <button onClick={() => handleAddGameElement()}>
-          Add {elementType}
-        </button>
-      </div>
+      <div className="add-element-wrapper">
+        <div className="add-element-form">
+          <select
+            name=""
+            id=""
+            onChange={handleSetFormGameId}
+            value={formGameId}
+          >
+            <option value="">Select Game</option>
+            {gamesList &&
+              gamesList.map((game) => (
+                <option key={game.game_id} value={game.game_id}>
+                  {game.name}
+                </option>
+              ))}
+          </select>
+          <input
+            type="text"
+            placeholder="name"
+            value={formName}
+            onChange={handleSetFormName}
+          />
+          <input
+            type="text"
+            placeholder="image-url"
+            value={formImgUrl}
+            onChange={handleSetFormImgUrl}
+          />
+          <textarea
+            name="form-description"
+            id="form-description"
+            placeholder="description"
+            value={formDescription}
+            onChange={handleSetFormDescription}
+          ></textarea>
+          <button onClick={() => handleAddGameElement()}>
+            Add {elementType}
+          </button>
+        </div>
 
-      <ElementCard
-        elementData={{
-          description: formDescription,
-          name: formName,
-          image_url: formImgUrl,
-          type_id: elementTypeId,
-          notes: [],
-        }}
-        viewType="card"
-        relationshipsList={[]}
-        typesList={typesList}
-      />
+        <ElementCard
+          elementData={{
+            description: formDescription,
+            name: formName,
+            image_url: formImgUrl
+              ? formImgUrl
+              : "https://www.svgrepo.com/show/508699/landscape-placeholder.svg",
+            type_id: elementTypeId,
+            notes: [],
+          }}
+          viewType="card"
+          relationshipsList={[]}
+          typesList={typesList}
+        />
+      </div>
       <ElementsList
         elementsList={elementsList}
         typesList={typesList}
