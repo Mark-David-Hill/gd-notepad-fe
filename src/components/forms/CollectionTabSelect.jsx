6faee16edd474feab6ce91921b74372fa53c292a
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-01-17 20:04:11 -0700

    sets up buttons to switch between tabs of the collection page

---
+++ src/components/forms/CollectionTabSelect.jsx
@@ -0,0 +1,26 @@
+const CollectionTabSelect = ({ currentTab, setCurrentTab }) => {
+  return (
+    <div className="view-select-wrapper">
+      <button
+        className={currentTab === "overview" ? "selected" : ""}
+        onClick={() => setCurrentTab("overview")}
+      >
+        Overview
+      </button>
+      <button
+        className={currentTab === "items" ? "selected" : ""}
+        onClick={() => setCurrentTab("items")}
+      >
+        Items
+      </button>
+      <button
+        className={currentTab === "types" ? "selected" : ""}
+        onClick={() => setCurrentTab("types")}
+      >
+        Types
+      </button>
+    </div>
+  );
+};
+
+export default CollectionTabSelect;
--- src/components/pages/Collection.jsx
+++ src/components/pages/Collection.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import CollectionTabSelect from "../forms/CollectionTabSelect";
 import SearchSection from "../game-elements/SearchSection";
 import ElementsList from "../game-elements/ElementsList";
 import AddElementForm from "../forms/AddElementForm";
@@ -14,6 +15,7 @@ export default function Collection() {
   const [selectedTypes, setSelectedTypes] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [collectionData, setCollectionData] = useState(null);
+  const [currentTab, setCurrentTab] = useState("overview");
 
   const { id } = useParams();
 
@@ -30,50 +32,59 @@ export default function Collection() {
 
   return (
     <div className="collection-container">
+      <CollectionTabSelect
+        currentTab={currentTab}
+        setCurrentTab={setCurrentTab}
+      />
+
       {collectionData ? (
         <div className="game-wrapper">
           <h1>{collectionData.name}</h1>
 
-          <div className="game-element-container">
-            <ItemCard
-              itemData={collectionData}
-              itemType="collection"
-              fetchRoute="collections"
-              // pageRoute="game-elements"
-              // viewType="page"
-            />
-          </div>
+          {currentTab === "overview" ? (
+            <div className="game-element-container">
+              <ItemCard
+                itemData={collectionData}
+                itemType="collection"
+                fetchRoute="collections"
+                // pageRoute="game-elements"
+                // viewType="page"
+              />
+            </div>
+          ) : currentTab === "items" ? (
+            <div className="items-container">
+              <SearchSection
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+                viewType={viewType}
+                setViewType={setViewType}
+                selectedElements={selectedElements}
+                setSelectedElements={setSelectedElements}
+                selectedTypes={selectedTypes}
+                setSelectedTypes={setSelectedTypes}
+              />
 
-          <div className="items-container">
-            <SearchSection
-              searchTerm={searchTerm}
-              setSearchTerm={setSearchTerm}
-              viewType={viewType}
-              setViewType={setViewType}
-              selectedElements={selectedElements}
-              setSelectedElements={setSelectedElements}
-              selectedTypes={selectedTypes}
-              setSelectedTypes={setSelectedTypes}
-            />
+              {/* <AddElementForm
+                setElementsList={setGameElements}
+                collectionsList={collections}
+                typesList={types}
+              />
 
-            {/* <AddElementForm
-            setElementsList={setGameElements}
-            collectionsList={collections}
-            typesList={types}
-          />
-
-          <div className={"items-wrapper"}>
-            <ElementsList
-              elementsList={gameElements}
-              currentRelationships={selectedElements}
-              viewType={viewType}
-              searchTerm={searchTerm}
-              currentTypes={selectedTypes}
-              currentCollections={selectedCollections}
-              currentRelatedElements={selectedElements}
-            />
-          </div> */}
-          </div>
+              <div className={"items-wrapper"}>
+                <ElementsList
+                  elementsList={gameElements}
+                  currentRelationships={selectedElements}
+                  viewType={viewType}
+                  searchTerm={searchTerm}
+                  currentTypes={selectedTypes}
+                  currentCollections={selectedCollections}
+                  currentRelatedElements={selectedElements}
+                />
+              </div> */}
+            </div>
+          ) : (
+            currentTab === "types" && <h1>Types</h1>
+          )}
         </div>
       ) : (
         <p>Loading...</p>
