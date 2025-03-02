7f05c4e8ade99ebcad51ba2eec8bcac2bf32c56a
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-03-02 09:34:08 -0700

    sets up logout function

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -3,10 +3,29 @@ import { useState, useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthContextProvider";
 
+import fetchWrapper from "../../lib/apiCall";
+
 export default function Navbar() {
-  const { isAuthenticated } = useContext(AuthContext);
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
 
   const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  async function logoutUser(email, password) {
+    const body = { email, password };
+
+    try {
+      const response = await fetchWrapper.apiCall(`/user/logout`, "POST", {});
+      console.log(response);
+      if (response.message === "Logout successful") {
+        setIsAuthenticated(false);
+        // navigate("/dashboard");
+      } else {
+        console.error("Logout failed:", response.message);
+      }
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  }
 
   return (
     <div className="navbar-container">
@@ -19,7 +38,9 @@ export default function Navbar() {
           Home
         </NavLink>
 
-        {!isAuthenticated && (
+        {isAuthenticated ? (
+          <button onClick={logoutUser}>Logout</button>
+        ) : (
           <NavLink
             to="/login"
             className={({ isActive }) => (isActive ? "active-link" : undefined)}
