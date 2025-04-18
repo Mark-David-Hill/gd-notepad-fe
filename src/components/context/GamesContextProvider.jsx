874260195f6e63a1a3465a039cc42fff196d0d78
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-04-17 19:54:56 -0600

    removes unused collections fetch and data from context

--- src/components/context/GamesContextProvider.jsx
+++ src/components/context/GamesContextProvider.jsx
@@ -5,13 +5,10 @@ import fetchWrapper from "../../lib/apiCall";
 export const GamesContext = createContext();
 
 export default function GamesContextProvider({ children }) {
-  const [collections, setCollections] = useState([]);
   const [types, setTypes] = useState([]);
   const [relationships, setRelationships] = useState([]);
 
   const gameDataState = {
-    collections,
-    setCollections,
     types,
     setTypes,
     relationships,
@@ -19,13 +16,6 @@ export default function GamesContextProvider({ children }) {
   };
 
   useEffect(() => {
-    fetchWrapper
-      .apiCall(`/collections`, "GET")
-      .then((response) => {
-        setCollections(response.results);
-      })
-      .catch((error) => console.error(`couldn't get collections`, error));
-
     fetchWrapper
       .apiCall(`/types`, "GET")
       .then((response) => {
