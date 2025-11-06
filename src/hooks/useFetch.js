4c401222bfd7368548bc02cdfc41c53455948759
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-11-05 22:18:57 -0700

    refactor useFetch hook to use useCallback and fixes typo

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 import fetchWrapper from "../lib/apiCall";
 
@@ -6,16 +6,18 @@ export default function useFetch(url) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const fetchData = () => {
+  const fetchData = useCallback(() => {
     setLoading(true);
     fetchWrapper
       .apiCall(url, "GET")
       .then((res) => setData(res.results))
-      .catch((err) => console.err(err.message || "Failed to fetch"))
+      .catch((err) => console.error(err.message || "Failed to fetch"))
       .finally(() => setLoading(false));
-  };
+  }, [url]);
 
-  useEffect(fetchData, [url]);
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return { data, loading };
 }
